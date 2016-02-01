59cf3f2063597b3c61c2448b3e9e47f297feee2b
Mohammad Asif <asif@likastic.com>
2016-02-01 10:49:44 -0800

    Model added on print button on message list

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,5 @@
 var app = angular.module('SMS_APP', ['ngMaterial']);
-app.controller('appCtrl' , function($scope , $http){
+app.controller('appCtrl' , function($scope , $http , $mdDialog){
 
     $scope.loading = true;
     $http.get('http://localhost:3000/allmessages').then(function(response){
@@ -8,4 +8,20 @@ app.controller('appCtrl' , function($scope , $http){
             $scope.messages = response.data;
         }  
     });
+
+    $scope.print = function(message) {
+    	console.log(message.content);
+    	$scope.message = message;
+        $mdDialog.show({
+            templateUrl:'../templates/dialogue.html',
+            locals : {'message' : $scope.message} ,
+            controller: function DialogController($scope, $mdDialog , message) {
+            	$scope.message=message;
+                $scope.cancel = function() {
+                	$mdDialog.hide();
+                }
+            } ,
+            clickOutsideToClose:true
+        });
+    }
 });
